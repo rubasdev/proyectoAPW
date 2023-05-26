861d24796ba3b48f3137b40519680f6fa86f6c1c
Rubén Lomeli <127460005+rubasdev@users.noreply.github.com>
2023-05-26 14:31:21 -0600

    Add files via upload

---
+++ proyectoAWEB/js/funtion.js
@@ -0,0 +1,12 @@
+function opcion1() {
+  document.getElementById("titulo").className = "red";
+  console.log("Seleccionaste la Opción 1 y es ROJO");
+}
+function opcion2() {
+  document.getElementById("titulo").className = "green";
+  console.log("Seleccionaste la Opción 2 y es VERDE");
+}
+function opcion3() {
+  document.getElementById("titulo").className = "blue";
+  console.log("Seleccionaste la Opción 3 y es Azul");
+}
